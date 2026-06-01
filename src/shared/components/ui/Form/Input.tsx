a5d273cf0ec5e625ef50f907741e435a95023fed
Mateus de Aquino <86917791+mateusaquinomr@users.noreply.github.com>
2026-06-01 10:12:58 -0300

    fix: corrige case do import do form.module.css

--- src/shared/components/ui/Form/Input.tsx
+++ src/shared/components/ui/Form/Input.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Form.module.css";
+// import styles from "./Form.module.css";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> { }
 
